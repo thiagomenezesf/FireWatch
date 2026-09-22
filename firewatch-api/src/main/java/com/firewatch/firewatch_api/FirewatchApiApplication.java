f36c320840276f47e41a49dbe18dbec233b66ac8
thiago <thifrancis1000@gmail.com>
2026-09-22 00:12:04 -0300

    Framework front e end

---
+++ firewatch-api/src/main/java/com/firewatch/firewatch_api/FirewatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.firewatch.firewatch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirewatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirewatchApiApplication.class, args);
+	}
+
+}
---
+++ firewatch-api/src/test/java/com/firewatch/firewatch_api/FirewatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firewatch.firewatch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirewatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
